bfb6cf251b47f8829f2b0b68c3e3e5294d3aa3c6
DoniaSalem7004184 <doniametwaly288@gmail.com>
2025-11-12 15:04:57 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,64 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
-
-#include <string>
+#include <iostream>
 using namespace std;
 
-//define animal class here 
+
+virtual class Animal{
+   
+    private:
+     string name;
+     int age;
+     bool isHungry;
+
+    Animal(string name, int age, bool isHungry){
+        name = name;
+        age = 0;
+        isHungry = 0;
+
+    }
+
+    class Mammal :: virtual Animal{
+
+        private:
+        string furColor;
+
+        Mammal(string furColor){
+            furColor = furColor;
+        }
+    }
+    
+    class Bird :: virtual Animal{
+            private:
+            float wingSpan;
+
+            Bird(float wingSpan){
+                wingSpan = wingSpan;
+            }
+
+        }
+
+    class Reptile : virtual Animal{
+            
+            private:
+            bool isVenomous;
+
+            Reptile (bool isVenomous){
+                isVenomous = 0;
+
+            }
+        }
+
+
+ void display(){ 
+
+    }
+
+void feed(){ 
+
+    }
 
 
 
-#endif
+
+
+};
+
